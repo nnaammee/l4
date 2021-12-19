e448f5b6b345e86937f0c0c3bcffe549a815585a
nnaammee <51121246+nnaammee@users.noreply.github.com>
2021-12-19 17:58:00 +0300

    Add files via upload

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <fstream>
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/stitching.hpp"
+
+using namespace std;
+using namespace cv;
+
+Stitcher::Mode mode = Stitcher::PANORAMA;
+
+vector<Mat> imgs;
+
+int main(int argc, char* argv[]) {
+    for (int i = 1; i < argc; ++i) {
+
+        Mat img = imread(argv[i]);
+
+        if (img.empty()){
+            cout << "Can't read image'" << argv[i] << "'\n";
+            return -1;
+        }
+        imgs.push_back(img);
+    }
+
+    Mat pano;
+    Ptr<Stitcher> stitcher = Stitcher::create(mode);
+
+    Stitcher::Status status = stitcher->stitch(imgs, pano);
+
+    if (status != Stitcher::OK) {
+        cout << "Can't stitch images\n";
+        return -1;
+    }
+
+    imwrite("result.jpg", pano);
+    imshow("Result", pano);
+    waitKey(0);
+
+    return 0;
+}
